bf070e5feea8fb21b9f2c9630688c9ad1b435059
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-03-12 12:44:36 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 136 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+	public class Main {
+		public static void main(String[] args) throws IOException {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			
+			int t = Integer.parseInt(br.readLine());
+			
+			for (int i = 1; i<=t; i++) {
+				for (int j = i; j<=t-1; j++) {
+					bw.write(" ");
+					
+
+				}
+				for ( int k = 1; k<=i; k++) {
+				    bw.write("*");
+				}
+				bw.write("\n");
+			}
+			bw.close();
+		}
+	}
