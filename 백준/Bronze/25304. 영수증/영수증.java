2d482d1cf721845e40ef135903cefd60cfe19991
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-03-12 12:59:05 +0900

    [Bronze V] Title: 영수증, Time: 220 ms, Memory: 17980 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,20 @@
+import java.util.*;
+    public class Main {
+        public static void main(String[] args){
+            Scanner scanner = new Scanner(System.in);
+            int x = scanner.nextInt();
+            int n = scanner.nextInt();
+            int sum = 0;
+            
+            for (int i = 1; i <= n; i++) {
+                int a = scanner.nextInt();
+                int b = scanner.nextInt();
+                sum += a*b;
+            }
+            if (x==sum) {
+            	System.out.println("Yes");
+            } else {
+            	System.out.println("No");
+            }
+        }
+    }
