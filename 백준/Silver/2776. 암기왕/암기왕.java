e4dcbb422b7df4eed69ee3fd565b3db8087404fb
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-04-01 22:14:21 +0900

    [Silver IV] Title: 암기왕, Time: 1572 ms, Memory: 286352 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -1,56 +1,34 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.HashSet;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
 		int t = Integer.parseInt(br.readLine());
 		
 		for (int i = 0; i < t; i++) {
 			int n = Integer.parseInt(br.readLine());
-			int[] list = new int[n];
-			
 			StringTokenizer str = new StringTokenizer(br.readLine());
 			
+			HashSet<Integer> list = new HashSet<Integer>();
+			
 			for (int j = 0; j < n; j++) {
-				list[j] = Integer.parseInt(str.nextToken());
+				list.add(Integer.parseInt(str.nextToken()));
 			}
 			
-			Arrays.sort(list);
-			
 			int m = Integer.parseInt(br.readLine());
-			
 			str = new StringTokenizer(br.readLine());
-						
-			 for (int k = 0; k < m; k++) {
-	                int target = Integer.parseInt(str.nextToken());
-	                int result = binarySearch(list, target);
-	                sb.append(result).append('\n');
-	            }
-	        }
-
-	        System.out.println(sb);
+			
+			for (int k = 0; k < m; k++) {
+				if (list.contains(Integer.parseInt(str.nextToken())) == true) {
+					sb.append(1).append("\n");
+				} else {
+					sb.append(0).append("\n");
+				}
+			}
 		}
-	
-	 public static int binarySearch(int[] arr, int target) {
-	        int low = 0;
-	        int high = arr.length - 1;
-
-	        while (low <= high) {
-	            int mid = (low + high) / 2;
-
-	            if (arr[mid] == target) {
-	                return 1;
-	            } else if (arr[mid] < target) {
-	                low = mid + 1;
-	            } else {
-	                high = mid - 1;
-	            }
-	        }
-
-	        return 0;
-	    }
+		System.out.println(sb);
+	}
 }
