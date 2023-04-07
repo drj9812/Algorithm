c575a6c877eceab951ea791340a00986c836656c
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-04-07 12:44:40 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 120 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -5,41 +5,15 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer tkn = new StringTokenizer(br.readLine());
-
+		
 		int x = Integer.parseInt(tkn.nextToken());
 		int y = Integer.parseInt(tkn.nextToken());
 		int w = Integer.parseInt(tkn.nextToken());
 		int h = Integer.parseInt(tkn.nextToken());
-
-		int minDistance = x;
-
-		if (x > y) {
-			minDistance = y;
-			if (minDistance > (w - x)) {
-				minDistance = w - x;
-				if (minDistance > (h - y)) {
-					minDistance = h - y;
-				}
-			} else {
-				if (minDistance > (h - y)) {
-					minDistance = h - y;
-				}
-			}
-
-		} else {
-			if (minDistance > (w - x)) {
-				minDistance = w - x;
-				if (minDistance > (h - y)) {
-					minDistance = h - y;
-				}
-			} else {
-				if (minDistance > (h - y)) {
-					minDistance = h - y;
-				}
-			}
-		}
-
-		System.out.println(minDistance);
-
+		
+		int minX = Math.min(x, w - x);
+		int minY = Math.min(y, h - y);
+		
+		System.out.println(Math.min(minX, minY));
 	}
 }
