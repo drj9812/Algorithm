f59c95910e6546027a30980d8eeaef04c3844a5c
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-04-20 00:36:37 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 156 ms, Memory: 15780 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.HashSet;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		HashSet<String> rainbowDancers = new HashSet<String>();
+		int n = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		
+		for (int i = 0; i < n; i++) {
+			String input = br.readLine();
+			String[] inputList = input.split(" ");
+			
+			if (inputList[0].equals("ChongChong") || inputList[1].equals("ChongChong")) {
+				rainbowDancers.add(inputList[0]);
+				rainbowDancers.add(inputList[1]);
+			}
+			
+			if (rainbowDancers.contains("ChongChong")) {
+				if (rainbowDancers.contains(inputList[0]) || rainbowDancers.contains(inputList[1])) {
+					rainbowDancers.add(inputList[0]);
+					rainbowDancers.add(inputList[1]);
+				}
+			}
+		}
+		
+		System.out.println(rainbowDancers.size());
+	}
+}
