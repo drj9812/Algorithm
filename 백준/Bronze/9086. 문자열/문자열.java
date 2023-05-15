6b55a6dbfcede6e91558b8808165993c829dd446
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-05-15 23:55:14 +0900

    [Bronze V] Title: 문자열, Time: 140 ms, Memory: 15884 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,14 +1,14 @@
 import java.io.*;
-	public class Main {
-		public static void main(String[] args) throws IOException {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			int a = Integer.parseInt(br.readLine());
-			
-			for (int i = 0; i < a; i++) {
-				String str = br.readLine();
-					System.out.print(str.charAt(0));
-					System.out.print(str.charAt(str.length()-1));
-					System.out.println();
-			}
-		}
-	}
+
+public class Main {
+public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    int n = Integer.parseInt(br.readLine());
+    
+    for (int i = 0; i < n; i++) {
+        String input = br.readLine();
+        System.out.println(input.charAt(0) + "" + input.charAt(input.length() - 1) + "");
+    }
+  }
+}
