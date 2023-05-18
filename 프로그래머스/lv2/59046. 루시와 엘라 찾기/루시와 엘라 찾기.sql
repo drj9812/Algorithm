54367a9a3a877d041c39a3038b4a792fbfcf7ec4
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-05-18 11:11:17 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,4 @@
+SELECT animal_id, name, sex_upon_intake
+  FROM animal_ins
+ WHERE name = 'Lucy' OR name = 'Ella' OR name = 'Pickle' OR name = 'Rogan' OR name = 'Sabrina' OR name = 'Mitty'
+ ORDER BY animal_id ASC;
