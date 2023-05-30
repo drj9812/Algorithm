dd7ba22b07cbcbae3f00cd5cc731f99c6bb2a79d
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-05-31 00:05:56 +0900

    [Silver V] Title: 나이순 정렬, Time: 1648 ms, Memory: 66808 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        Member[] members = new Member[n];
+
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            int age = Integer.parseInt(input[0]);
+            String name = input[1];
+            members[i] = new Member(age, name);
+        }
+
+        Arrays.sort(members, new Comparator<Member>() {
+            @Override
+            public int compare(Member m1, Member m2) {
+                if (m1.age == m2.age) {
+                    return m1.index - m2.index;
+                } else {
+                    return m1.age - m2.age;
+                }
+            }
+        });
+
+        for (Member member : members) {
+            System.out.println(member.age + " " + member.name);
+        }
+    }
+
+    static class Member {
+        int age;
+        String name;
+        int index;
+
+        public Member(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+    }
+}
