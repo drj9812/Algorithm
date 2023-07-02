92b46c83830375458a96449a8abffdc2fd556b87
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-07-03 01:36:41 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,9 +1,9 @@
 SELECT name, datetime
   FROM (
-SELECT rownum, i.name, i.datetime, o.animal_id
+SELECT ROWNUM, i.name, i.datetime, o.animal_id
   FROM animal_ins i
   LEFT JOIN animal_outs o
     ON i.animal_id = o.animal_id
  ORDER BY i.datetime 
   )
- WHERE animal_id IS NULL and rownum <= 3;
+ WHERE animal_id IS NULL AND ROWNUM <= 3;
