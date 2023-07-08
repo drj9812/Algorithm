04a809b6123813a1c115710cb94f4ead4a1aea8c
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-07-08 16:26:16 +0900

    [unrated] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,14 @@
+SELECT '/home/grep/src' ||
+       '/' ||
+       board_id ||
+       '/' ||
+       file_id ||
+       file_name || file_ext AS file_path
+  FROM used_goods_file
+ WHERE board_id = (
+    SELECT board_id
+      FROM (SELECT board_id, views
+              FROM used_goods_board
+             ORDER BY views DESC)
+     WHERE ROWNUM = 1)
+ ORDER BY file_id DESC;
