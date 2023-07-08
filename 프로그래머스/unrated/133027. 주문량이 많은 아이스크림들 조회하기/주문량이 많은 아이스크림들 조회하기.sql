6e3cfe446273dbc9af43012569c1d34ae94b6412
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-07-09 02:41:00 +0900

    [unrated] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -0,0 +1,12 @@
+SELECT FLAVOR
+  FROM
+    (SELECT flavor, SUM(total_order) AS total_order
+       FROM
+         (SELECT flavor, total_order
+            FROM first_half
+           UNION ALL
+          SELECT flavor, total_order
+            FROM july)
+      GROUP BY flavor
+      ORDER BY total_order DESC)
+ WHERE ROWNUM <= 3; 
