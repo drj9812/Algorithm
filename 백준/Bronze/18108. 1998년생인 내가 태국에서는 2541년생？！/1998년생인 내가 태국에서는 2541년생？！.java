1a77d58f10d66d9e92350db385b249173be9d776
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-12-02 16:45:20 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 204 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -1,8 +1,12 @@
-import java.util.*;
-    public class Main {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            int a = scanner.nextInt();
-            System.out.println(a-543);
-        }
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int input = sc.nextInt();
+        int result = input - 543;
+        
+        System.out.println(result);
     }
+}
