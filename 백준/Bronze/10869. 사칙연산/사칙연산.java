29f794b46ab7b3ea7d26f67d5005e9e2ecc34ecd
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-12-02 16:41:00 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Bronze/10869. 사칙연산/사칙연산.java
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -1,14 +1,22 @@
-import java.util.* ;
-    public class Main {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            System.out.println(a+b);
-            System.out.println(a-b);
-            System.out.println(a*b);
-            System.out.println(a/b);
-            System.out.println(a%b);
-            
-        }
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int plus = a + b;
+        int subtract = a - b;
+        int multiply = a * b;
+        int devide = a / b;
+        int remainder = a % b;
+        
+        System.out.println(plus);
+        System.out.println(subtract);
+        System.out.println(multiply);
+        System.out.println(devide);
+        System.out.println(remainder);
     }
+} 
