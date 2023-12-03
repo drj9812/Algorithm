f3688355f24b8fdb6021eacc7f8c9db38a2fab6a
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-12-03 12:40:06 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,20 +1,21 @@
-import java.util.*;
-    public class Main {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            int a = scanner.nextInt();
-            if (90 <= a && a <= 100) {
-                System.out.println("A");
-            } else if (80 <= a && a <= 89) {
-                System.out.println("B");
-            } else if (70 <= a && a <= 79) {
-                System.out.println("C");
-            } else if (60 <= a && a <= 69) {
-                System.out.println("D");
-            } else {
-                System.out.println("F");
-            }
-                
-            
-        }
-    }
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int score = sc.nextInt();
+		
+		if (score >= 90) {
+			System.out.println("A");
+		} else if (score >= 80) {
+			System.out.println("B");
+		} else if (score >= 70) {
+			System.out.println("C");
+		} else if (score >= 60) {
+			System.out.println("D");
+		} else {
+			System.out.println("F");
+		}
+	}
+}
