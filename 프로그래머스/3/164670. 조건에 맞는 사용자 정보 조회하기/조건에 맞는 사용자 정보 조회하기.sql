03876bb3d8164a1f819c26ca942664502fa1be7f
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-12-09 18:43:04 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,10 @@
+SELECT user_id,
+       nickname,
+       city || ' ' || street_address1 || ' ' || street_address2 AS 전체주소,
+       REGEXP_REPLACE(TO_CHAR(tlno), '(\d{3})(\d{4})(\d{4})', '\1-\2-\3') AS 전화번호
+  FROM used_goods_user
+ WHERE user_id IN (SELECT writer_id
+                     FROM used_goods_board
+                    GROUP BY writer_id
+                   HAVING COUNT(writer_id) >= 3)
+ ORDER BY user_id DESC;
