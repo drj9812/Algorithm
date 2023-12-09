0a86b7eca17027e75f6996f49dcb1f0af8e84e1e
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-12-09 21:51:14 +0900

    [Bronze V] Title: 합, Time: 204 ms, Memory: 17720 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,12 +1,16 @@
-import java.util.*;
-    public class Main {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            int a = scanner.nextInt();
-            int sum = 0;
-            for (int i = 1;i <= a;i++) {
-                sum += i;
-            }
-            System.out.println(sum);
-        }
-    }
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int sum = 0;
+		
+		for (int i = 1; i <= n; i++) {
+			sum += i;
+		}
+		
+		System.out.println(sum);
+	}
+}
