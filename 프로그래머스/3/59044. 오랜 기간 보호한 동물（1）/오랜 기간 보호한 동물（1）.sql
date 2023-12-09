ff38759da65c442488f003f680377098c8f6d395
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-12-09 17:15:52 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,10 +1,7 @@
 SELECT name, datetime
-  FROM (
-SELECT i.name, i.datetime
-  FROM animal_ins i
-  LEFT JOIN animal_outs o
-    ON i.animal_id = o.animal_id
- WHERE o.animal_id IS NULL
- ORDER BY i.datetime 
-  )
- WHERE ROWNUM <= 3;
+  FROM (SELECT i.name, i.datetime, o.animal_id
+          FROM animal_ins i
+          LEFT JOIN animal_outs o
+            ON i.animal_id = o.animal_id
+         ORDER BY i.datetime)
+ WHERE animal_id IS NULL AND ROWNUM <= 3;
