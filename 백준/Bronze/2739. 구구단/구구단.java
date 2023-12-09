d0b17c3e6ca9fb7b51e943fb27b61c66855bedd3
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-12-09 21:11:21 +0900

    [Bronze V] Title: 구구단, Time: 236 ms, Memory: 18532 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,11 +1,13 @@
-import java.util.*;
-    public class Main {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            int a = scanner.nextInt();
-            
-            for (int i=1;i<=9;i++) {
-                System.out.println(a+" * "+i+" = "+a*i);
-            }
-        }
-    }
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for (int i = 1; i < 10; i++) {
+			System.out.println(n + " * " + i + " = " + n * i);
+		}
+	}
+}
