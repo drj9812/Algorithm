f7dc64fd59aa871402afdcefed280db9179739d9
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-12-10 16:54:05 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 852 ms, Memory: 235876 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -1,19 +1,26 @@
 import java.io.*;
 import java.util.StringTokenizer;
-    public class Main {
-        public static void main(String[] args) throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-            StringTokenizer str;
-            int t = Integer.parseInt(br.readLine());
-            
-            for (int i = 1; i<=t; i++) {
-                str = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(str.nextToken());
-                int b = Integer.parseInt(str.nextToken());
-                
-                bw.write(a+b+"\n");
-            }
-            bw.close();
-        }
-    }
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int sum = 0;
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < t; i++) {
+			StringTokenizer tkn = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(tkn.nextToken());
+			int b = Integer.parseInt(tkn.nextToken());
+			
+			sum = a + b;
+			
+			bw.write(sum + "\n");
+		}
+        br.close();
+		bw.flush();
+		bw.close();
+	}
+}
