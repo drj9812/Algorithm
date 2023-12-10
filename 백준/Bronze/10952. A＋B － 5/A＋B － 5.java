fca67379d0274e319d4ece77e5636ac1a03b2382
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-12-10 17:45:48 +0900

    [Bronze V] Title: A+B - 5, Time: 144 ms, Memory: 14604 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,23 +1,30 @@
 import java.io.*;
 import java.util.StringTokenizer;
-	public class Main {
-		public static void main(String[] args) throws IOException {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-			StringTokenizer str;	
 
-			while (true) {
-				str = new StringTokenizer(br.readLine());	
-			    int a = Integer.parseInt(str.nextToken());
-			    int b = Integer.parseInt(str.nextToken());
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer tkn;
 
-			    if (a+b==0) {
-			    	break;
-			    }
-			    bw.write(a+b+"\n");
+		int sum = 0;
 
+		while (true) {
+			tkn = new StringTokenizer(br.readLine());
+
+			int a = Integer.parseInt(tkn.nextToken());
+			int b = Integer.parseInt(tkn.nextToken());
+
+			sum = a + b;
+
+			if (a == 0 && b == 0) {
+				break;
 			}
-			br.close();
-			bw.close();
+			
+			bw.write(sum + "\n");
 		}
+		bw.flush();
+		bw.close();
+		br.close();
 	}
+}
