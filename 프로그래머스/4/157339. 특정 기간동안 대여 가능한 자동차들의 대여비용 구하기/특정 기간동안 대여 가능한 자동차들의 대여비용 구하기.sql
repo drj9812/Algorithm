8b10e7b8f3fa4c29666a8d9f6e84964e804f8276
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-12-23 23:29:10 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,19 +1,18 @@
-SELECT car_id, car_type, daily_fee * 30 - (daily_fee * 30 * discount_rate / 100) AS fee
-  FROM (SELECT c.car_id,
-               c.car_type,
-               c.daily_fee,
-               d.discount_rate
-          FROM car_rental_company_car c
-         INNER JOIN car_rental_company_discount_plan d
-            ON c.car_type = d.car_type
-         WHERE c.car_id NOT IN (SELECT car_id
-                                  FROM car_rental_company_rental_history
-                                 WHERE end_date > DATE '2022-11-01' AND start_date < DATE '2022-12-01')
-               AND
-               c.car_type IN ('세단', 'SUV')
-               AND
-               d.duration_type = '30일 이상')
- WHERE daily_fee * 30 - (daily_fee * 30 * discount_rate / 100) >= 500000
+SELECT c.car_id,
+       c.car_type,
+       c.daily_fee * 30 - (c.daily_fee * 30 * d.discount_rate / 100) AS fee
+  FROM car_rental_company_car c
+ INNER JOIN car_rental_company_discount_plan d ON c.car_type = d.car_type
+ WHERE c.car_type IN ('세단', 'SUV')
        AND
-       daily_fee * 30 - (daily_fee * 30 * discount_rate / 100) < 2000000
- ORDER BY fee DESC, car_type ASC, car_id DESC;
+       d.duration_type = '30일 이상'
+       AND NOT EXISTS (SELECT 1
+                         FROM car_rental_company_rental_history r
+                        WHERE c.car_id = r.car_id
+                              AND
+                              r.end_date > DATE '2022-11-01'
+                              AND
+                              r.start_date < DATE '2022-12-01')
+  AND c.daily_fee * 30 - (c.daily_fee * 30 * d.discount_rate / 100) BETWEEN 500000 AND 1999999
+ORDER BY
+  fee DESC, car_type ASC, car_id DESC;
