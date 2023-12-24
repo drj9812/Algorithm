dacaa9b70bc16772627db702394f911b6290fedf
drj9812 <126648227+drj9812@users.noreply.github.com>
2023-12-24 15:13:26 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,17 +1,18 @@
-SELECT sub.history_id, sub.fee - sub.fee * NVL(d.discount_rate, 0) / 100 AS fee
+SELECT a.history_id,
+       CASE WHEN a.period = '7일 미만' THEN a.fee
+            ELSE a.fee - (a.fee * b.discount_rate / 100)
+        END AS fee
   FROM (SELECT h.history_id,
                c.daily_fee * (h.end_date - h.start_date + 1) AS fee,
                CASE WHEN h.end_date - h.start_date + 1 >= 90 THEN '90일 이상'
                     WHEN h.end_date - h.start_date + 1 >= 30 THEN '30일 이상'
                     WHEN h.end_date - h.start_date + 1 >= 7 THEN '7일 이상'
-               ELSE '7일 미만'
-               END AS duration_type
+                    ELSE '7일 미만'
+                END AS period
           FROM car_rental_company_car c
          INNER JOIN car_rental_company_rental_history h
             ON c.car_id = h.car_id
-         WHERE car_type = '트럭') sub
-  LEFT JOIN car_rental_company_discount_plan d
-    ON sub.duration_type = d.duration_type
-       AND
-       d.car_type = '트럭'
- ORDER BY fee DESC, sub.history_id DESC;
+         WHERE c.car_type = '트럭') a
+  LEFT JOIN car_rental_company_discount_plan b
+    ON b.car_type = '트럭' AND b.duration_type = a.period
+ ORDER BY fee DESC, history_id DESC;
