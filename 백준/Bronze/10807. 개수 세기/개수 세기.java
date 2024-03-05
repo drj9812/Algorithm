fe4a8bbec67eae69e384b4f5c8c23b502d38ac4d
drj9812 <126648227+drj9812@users.noreply.github.com>
2024-03-06 02:58:57 +0900

    [Bronze V] Title: 개수 세기, Time: 120 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,19 +1,26 @@
 import java.io.*;
-import java.util.*;
-   public class Main {
-      public static void main(String[] args) throws IOException {
-         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-         int n = Integer.parseInt(br.readLine());
-         StringTokenizer str = new StringTokenizer(br.readLine());
-         int L = Integer.parseInt(br.readLine());
-         int cnt = 0;
-         
-         for (int i=0; i<n; i++) {
-            int b = Integer.parseInt(str.nextToken());
-            if(b==L) {
-               cnt ++;
-            }
-         }
-         System.out.println(cnt);
-      }
-   }
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer tkn;
+		int cnt = 0;
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] nArray = new int[n];
+		
+		tkn = new StringTokenizer(br.readLine());
+		int target = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < nArray.length; i++) {
+			nArray[i] = Integer.parseInt(tkn.nextToken());
+			
+			if (nArray[i] == target) {
+				cnt++;
+			}
+		}
+		
+		System.out.println(cnt);
+	}
+}
