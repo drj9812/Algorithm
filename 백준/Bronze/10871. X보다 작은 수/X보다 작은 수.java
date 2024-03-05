e7b22da85de8be0296e73f843da1793659187328
drj9812 <126648227+drj9812@users.noreply.github.com>
2024-03-06 03:11:59 +0900

    [Bronze V] Title: X보다 작은 수, Time: 196 ms, Memory: 17336 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,19 +1,26 @@
-import java.util.*;
-	public class Main {
-		public static void main(String[] args) {
-			Scanner scanner = new Scanner(System.in);
-			int a = scanner.nextInt();
-			int[] iArr = new int[a];
-			int b = scanner.nextInt();
-			
-			for (int i=0; i<a; i++) {
-				iArr[i]=scanner.nextInt();
-				if (iArr[i]<b) {
-					System.out.print(iArr[i]+" ");
-				}
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer tkn = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(tkn.nextToken());
+		int[] nArray = new int[n];
+
+		int x = Integer.parseInt(tkn.nextToken());
+		tkn = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < n; i++) {
+			nArray[i] = Integer.parseInt(tkn.nextToken());
+		
+			if (nArray[i] < x) {
+				sb.append(nArray[i] + " ");
 			}
-			
-			
-			
 		}
+		
+		System.out.println(sb.toString());
 	}
+}
