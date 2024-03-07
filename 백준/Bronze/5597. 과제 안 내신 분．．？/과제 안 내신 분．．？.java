f885c649f61bd68b9781c4c6df2a89a2c6aa4203
drj9812 <126648227+drj9812@users.noreply.github.com>
2024-03-07 12:57:09 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 120 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,20 +1,20 @@
 import java.io.*;
-	public class Main {
-		public static void main(String[] args) throws IOException {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			
-			boolean[] bArr = new boolean[31];
-			
-			for (int i = 0; i < 28; i++) {
-				int student = Integer.parseInt(br.readLine());
-				bArr[student] = true;
-				
-			}
-			
-			for (int j = 1; j < bArr.length; j++) {
-				if (bArr[j]==false) {
-					System.out.println(j);
-				}
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		boolean[] arr = new boolean[30];
+		
+		for (int i = 0; i < 28; i++) {
+			int num = Integer.parseInt(br.readLine());
+			arr[num - 1] = true;
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == false) {
+				System.out.println(i + 1);
 			}
 		}
 	}
+}
