10a1fb0d6e0427d43bfef4744bdaf5ead1050c12
drj9812 <126648227+drj9812@users.noreply.github.com>
2024-03-07 13:18:27 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,27 +1,16 @@
 import java.io.*;
-	public class Main {
-		public static void main(String[] args) throws IOException {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-			boolean[] bArr = new boolean[42];
-			int cnt = 0;
-			
-			for (int i = 0; i < 10; i++) {
-				bArr[Integer.parseInt(br.readLine())%42] = true;
-			}
-			
-			for (int j = 0; j < bArr.length; j++) {
-				if (bArr[j]==true) {
-					cnt++;
-				}
-			}
-//			br.close();
-//			bw.flush();
-//			bw.write(cnt);
-//			bw.close();
-//			
-			
-			System.out.println(cnt);
+import java.util.HashSet;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		HashSet<Integer> remainderSet = new HashSet<>();
+		
+		for (int i = 0; i < 10; i++) {
+			remainderSet.add(Integer.parseInt(br.readLine()) % 42);
 		}
+		
+		System.out.println(remainderSet.size());
 	}
-	
+}
