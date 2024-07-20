bedc117de48eb3e395084c82cce6220da7e93c05
drj9812 <126648227+drj9812@users.noreply.github.com>
2024-07-20 16:36:49 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,13 @@
+const solution = (price) => {
+    if (price >= 500000) {
+        return Math.trunc(price - price * 0.2);
+        
+    } else if (price >= 300000) {
+        return Math.trunc(price - price * 0.1);
+        
+    } else if (price >= 100000) {
+        return Math.trunc(price - price * 0.05);
+    } else {
+        return price;
+    }
+ }
