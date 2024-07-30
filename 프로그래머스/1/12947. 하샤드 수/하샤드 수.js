a8c55a8f65c14ba2642869fedb5144f7aeac02bb
drj9812 <126648227+drj9812@users.noreply.github.com>
2024-07-30 11:32:51 +0900

    [level 1] Title: 하샤드 수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,16 +1,5 @@
-// const solution = (x) => (x + '').split('').reduce((prev, curr) => {
-// //    const sum = (Number(prev) + Number(curr));
-   
-// //    return x % sum === 0 ? true : false;
-//    return x % (Number(prev) + Number(curr)) ? false : true;
-// })
-
-// // const solution = (x) => (x + '').split('').reduce((prev, curr) => {
-// //    return x % (Number(prev) + Number(curr)) === 0 ? true : false;
-// // })
-
 const solution = (x) => {
-    const sumOfDigits = (x + '').split('').reduce((prev, curr) => Number(prev) + Number(curr));
+    const sum = (x + '').split('').reduce((prev, curr) => Number(prev) + Number(curr));
     
-    return x % sumOfDigits === 0;
+    return x % sum === 0;
 };
