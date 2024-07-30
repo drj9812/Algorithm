bfe0fe249186b3a32f08dce489f5378df40a85b8
drj9812 <126648227+drj9812@users.noreply.github.com>
2024-07-30 12:46:45 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,14 +1,1 @@
-const solution = (s1, s2) => {
-    let count = 0;
-
-    s1.forEach(item1 => {
-        s2.forEach(item2 => {
-
-            if (item1 === item2) {
-                count++;
-            }
-        });
-    });
-
-    return count;
-};
+const solution = (s1, s2) => s1.filter((e) => s2.includes(e)).length;
