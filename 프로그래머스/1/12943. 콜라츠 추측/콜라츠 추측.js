deec40caa3ae165d24168801e49aa4f3fa6fa37d
drj9812 <126648227+drj9812@users.noreply.github.com>
2024-07-31 17:04:15 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,18 +1,10 @@
 const solution = (n) => {
     let count = 0;
     
-    while (n !== 1) {
-        if (n % 2 === 0) {
-            n = n / 2;
-            count++;
-        } else {
-            n = n * 3 + 1;
-            count ++;
-        }
-        
-        if (count === 500) {
-            return -1;
-        }
+    while (n !== 1 && count !== 500) {
+        n % 2 === 0 ? n = n / 2 : n = n * 3 + 1;
+        count++;
     }
-    return count;
+
+    return count === 500 ? -1 : count;
 };
