7cec2e4b0903385116da546c67431c25212418e6
drj9812 <126648227+drj9812@users.noreply.github.com>
2024-07-31 16:43:56 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.48 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -2,6 +2,6 @@ const solution = (my_string) => {
 
     return my_string.split('').map((e) => {
 
-        return e === e.toUpperCase() && e !== e.toLowerCase() ? e.toLowerCase() : e.toUpperCase();
+        return e === e.toUpperCase() ? e.toLowerCase() : e.toUpperCase();
     }).join('');
 };
