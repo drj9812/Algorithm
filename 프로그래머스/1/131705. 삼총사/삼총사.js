7287195b57533f26fa2ccc3db873986a09ce545c
drj9812 <126648227+drj9812@users.noreply.github.com>
2024-08-01 18:18:13 +0900

    [level 1] Title: 삼총사, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,16 @@
+const solution = (number) => {
+    let sum = 0;
+    let count = 0;
+
+    for (let i = 0; i < number.length; i++) {
+        for (let j = i + 1; j < number.length; j++) {
+            for (let k = j + 1; k < number.length; k++) {
+                
+                if (number[i] + number[j] + number[k] === 0) {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+};
