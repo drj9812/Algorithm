04c3e64e35607fc42791a8f2892f76806cd3a109
drj9812 <126648227+drj9812@users.noreply.github.com>
2024-09-13 17:36:12 +0900

    [level 1] Title: 과일 장수, Time: 243.14 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/README.md
+++ 프로그래머스/1/135808. 과일 장수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 과일 장수 - 135808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135808) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135808#) 
 
 ### 성능 요약
 
-메모리: 83.3 MB, 시간: 230.56 ms
+메모리: 82.3 MB, 시간: 243.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 13일 16:55:47
+2024년 09월 13일 17:36:05
 
 ### 문제 설명
 
--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,26 +1,9 @@
-// const solution = (k, m, score) => {
-//     return score.sort((a, b) => a - b)
-//         .reduce((acc, curr, i) => {
-//             if (i >= score.length % m && i % m === 0) acc += curr * m;
-        
-//             return acc;
-//         }, 0);
-// };
-
 const solution = (k, m, score) => {
-    // 사과 점수를 내림차순으로 정렬
-    score.sort((a, b) => b - a);
-    
-    let total = 0;
-    // 상자를 포장할 수 있는 개수
-    const numBoxes = Math.floor(score.length / m);
-    
-    // 각 상자에 대해 최저 점수를 계산하여 총 이익을 계산
-    for (let i = 0; i < numBoxes; i++) {
-        // 현재 상자의 최저 점수는 현재 점수 배열의 (i * m + m - 1) 인덱스에 위치
-        const minScore = score[i * m + m - 1];
-        total += minScore * m;
-    }
-    
-    return total;
+    return score.sort((a, b) => b - a)
+        .reduce((acc, curr, i) => {
+            if (i % m === m - 1) {
+                acc += curr * m;
+            }
+            return acc;
+    }, 0);
 };
