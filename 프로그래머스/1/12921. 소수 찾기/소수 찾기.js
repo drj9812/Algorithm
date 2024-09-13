44bfa3c6a43899cf0355f5b89323a146fb87474d
drj9812 <126648227+drj9812@users.noreply.github.com>
2024-09-13 19:13:50 +0900

    [level 1] Title: 소수 찾기, Time: 31.46 ms, Memory: 45.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,23 @@
+const solution = (n) => {
+    return sieveOfEratosthenes(n).length;
+}
+
+function sieveOfEratosthenes(limit) {
+    const isPrime = Array(limit + 1).fill(true);
+    isPrime[0] = isPrime[1] = false;
+
+    for (let i = 2; i * i <= limit; i++) {
+        if (isPrime[i]) {
+            for (let j = i * i; j <= limit; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    const primes = [];
+    for (let i = 2; i <= limit; i++) {
+        if (isPrime[i]) primes.push(i);
+    }
+
+    return primes;
+}
