23b1fb4c2d72d641ba4d6396f6eb5899f0683164
drj9812 <126648227+drj9812@users.noreply.github.com>
2024-09-16 19:52:29 +0900

    [level 1] Title: 옹알이 (2), Time: 0.36 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,27 +1,25 @@
-function solution(babbling) {
+const solution = (babbling) => {
     const possibleSounds = ['aya', 'ye', 'woo', 'ma'];
 
     const isValidBabbling = (str) => {
-        let prevSounds = '';
+        let prevSound = '';
 
         while (str.length > 0) {
             let matched = false;
 
             for (const sound of possibleSounds) {
                 if (!str.startsWith(sound)) continue;
-                // 같은 발음이 연속된 경우
-                if (sound === prevSounds) return false;
-                prevSounds = sound;
+                if (sound === prevSound) return false;
+
+                prevSound = sound;
                 str = str.slice(sound.length);
                 matched = true;
+
                 break;
             }
-            // 발음할 수 없는 문자열이 포함된 경우
             if (!matched) return false;
         }
         return true;
     };
-
-    // babbling 배열에서 유효한 발음 문자열의 개수 계산
     return babbling.filter(isValidBabbling).length;
 }
