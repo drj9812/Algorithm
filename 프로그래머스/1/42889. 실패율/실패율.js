3e529181efc9878465d1a6ed37ae0c57059487e6
drj9812 <126648227+drj9812@users.noreply.github.com>
2024-09-16 16:36:08 +0900

    [level 1] Title: 실패율, Time: 70.83 ms, Memory: 49.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -0,0 +1,25 @@
+const solution = (N, stages) => {
+    const stageFailureRates = [];
+    const stageCounts = new Map();
+    let remainingPlayers = stages.length;
+
+    stages.sort((a, b) => a - b);
+    for (const stage of stages) {
+        stageCounts.get(stage) === undefined
+            ? stageCounts.set(stage, 1)
+            : stageCounts.set(stage, stageCounts.get(stage) + 1);
+    }
+
+    for (let stage = 1; stage <= N; stage++) {
+        if (stageCounts.get(stage) !== undefined) {
+            stageFailureRates.push([stage, stageCounts.get(stage) / remainingPlayers]);
+            remainingPlayers -= stageCounts.get(stage);
+        } else {
+            stageFailureRates.push([stage, 0]);
+        }
+    }
+
+    stageFailureRates.sort((a, b) => b[1] - a[1] || a[0] - b[0]);
+
+    return stageFailureRates.map(([stage, _]) => stage);
+};
